26bcd40f87351d881e6de8e29a31e8afabdd1ac5
johannes-ackermann <104435731+johannes-ackermann@users.noreply.github.com>
2022-05-03 13:10:49 +0200

    Add files via upload

---
+++ test-application.py
@@ -0,0 +1,23 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import json
+import glob
+from pprint import pprint
+import streamlit as st
+import pandas as pd
+
+st.header("Political Party Classification")
+st.write("""Hello! This is Simon, Johannes and Dominik. Our aim is to predict the affiliation to a political party from Tweets. """)
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+
+
+
