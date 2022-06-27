6ce183c785d96d0d430cb0785d35aeabf1e84cf8
JohannesA15 <104435731+JohannesA15@users.noreply.github.com>
2022-06-27 13:59:36 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import streamlit as st
-import json as js
 
 st.title('Political Party Classification')
 
@@ -14,26 +7,3 @@ st.header('Erste Aufgabe: Vorstellung')
 st.subheader('Unser Team')
 
 st.text('Hallo zusammen,\nwir sind Johannes Ackermann, Simon Müller und Dominik Rolof.\nWir studieren Wirtschaftsinfo und wünschen uns allen ein erfolgreiches Semester.\nUnser Projekt befasst sich mit der Klassifizierung\neinzelner Politiker in Parteien anhand ihrer Tweets')
-
-file = open('Philipp_Amthor.jl')
-data = js.load(file)
-
-if st.button('Beispiel eines Datensatzes'):
-    st.markdown(data)
-option = st.selectbox('Möchtest du sehen, wem der Datensatz gehört?', ('Bitte auswählen...', 'Ja', 'Nein'))
-if (option == 'Ja'):
-    st.text('Du wolltest es nicht anders...')
-    st.image('https://img-9gag-fun.9cache.com/photo/aGgXrwz_460s.jpg')
-elif (option == 'Bitte auswählen...'):
-    st.text('Bitte wähle Ja oder Nein')
-else:
-    st.text('Dann halt nicht...')
-
-file.close()
-
-
-# In[ ]:
-
-
-
-
